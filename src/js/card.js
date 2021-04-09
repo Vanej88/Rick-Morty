16e3655f2f138fff9a539178237ac0766ab2f4cb
Vanej88 <vanej88@gmail.com>
2021-04-09 21:11:59 +0200

    complete version(desktop y responsive)

--- src/js/card.js
+++ src/js/card.js
@@ -15,8 +15,21 @@ function displayCard(character) {
                             <img class="card__img" src="${character.image}" data-image="${character.image}" alt="rick">
                         </div>
                         <div class="card__box-text">
-                            <p class="card__title" data-name="${character.name}">${character.name}</p>
-                            <p class="card__description"><span class="card__circle card__circle--active"></span><span class="card__status" data-status="${character.status}">${character.status}</span>-<span class="card__type" data-species="${character.species}" data-gender="${character.gender}" data-origin="${character.origin.name}" data-location="${character.location.name}">${character.species}</span></p>
+                            <p class="card__title" 
+                              data-name="${character.name}">${character.name}</p>
+                            <div class="card__description">
+                              <p class="card__status">
+                                <span class="card__status-circle card__status-circle--active card__circle card__circle--active"></span>
+                                <span data-status="${character.status}">${character.status}</span>
+                              </p>
+                              
+                              <p class="card__type" 
+                                    data-species="${character.species}" 
+                                    data-gender="${character.gender}" 
+                                    data-origin="${character.origin.name}" 
+                                    data-location="${character.location.name}">${character.species}
+                              </p>
+                            </div>
                         </div>   
                     </div>
 `;
